216061d6e7ff3a3f050b6b0483bcce4c4855db5a
Quentin Curteman <qcurteman@gmail.com>
2018-06-30 08:34:46 -0700

    Added google-aiy page

---
+++ app/Http/Controllers/GoogleAiyController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GoogleAiyController extends Controller
+{
+    public function main_page()
+    {
+        return view('google-aiy');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/google-aiy', [
+    'uses' => 'GoogleAiyController@main_page'
+]);
